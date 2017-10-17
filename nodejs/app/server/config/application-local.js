b376e137c2d6c6018e781713757b6d09ac575656
Natasha D'Silva <ndsilva@ca.ibm.com>
2017-10-17 14:37:32 -0400

    update link

--- nodejs/app/server/config/application-local.js
+++ nodejs/app/server/config/application-local.js
@@ -36,30 +36,10 @@ const server = {
 // FILL IN with your Streaming Analytic bluemix credential
 const streaming_analytics =
 {
-  "password": "f74aedc1-81af-497b-a781-cc45018dda75",
-  "rest_port": "443",
-  "bundles_path": "/jax-rs/bundles/service_instances/d99131f8-76e0-45e1-86de-c854979235d1/service_bindings/e75ed0f0-8424-402f-9201-94dee9ec7838",
-  "resources_path": "/jax-rs/resources/service_instances/d99131f8-76e0-45e1-86de-c854979235d1/service_bindings/e75ed0f0-8424-402f-9201-94dee9ec7838",
-  "stop_path": "/jax-rs/streams/stop/service_instances/d99131f8-76e0-45e1-86de-c854979235d1/service_bindings/e75ed0f0-8424-402f-9201-94dee9ec7838",
-  "rest_host": "streams-app-service.ng.bluemix.net",
-  "size_path": "/jax-rs/streams/size/service_instances/d99131f8-76e0-45e1-86de-c854979235d1/service_bindings/e75ed0f0-8424-402f-9201-94dee9ec7838",
-  "jobs_path": "/jax-rs/jobs/service_instances/d99131f8-76e0-45e1-86de-c854979235d1/service_bindings/e75ed0f0-8424-402f-9201-94dee9ec7838",
-  "start_path": "/jax-rs/streams/start/service_instances/d99131f8-76e0-45e1-86de-c854979235d1/service_bindings/e75ed0f0-8424-402f-9201-94dee9ec7838",
-  "rest_url": "https://streams-app-service.ng.bluemix.net",
-  "userid": "cdc97d15-2f3f-4268-8f9e-c0221727c29d",
-  "status_path": "/jax-rs/streams/status/service_instances/d99131f8-76e0-45e1-86de-c854979235d1/service_bindings/e75ed0f0-8424-402f-9201-94dee9ec7838"
-}
+  }
 // FILL IN with your IOT bluemix credential
 const iot_platform = {
-  "iotCredentialsIdentifier": "a2g6k39sl6r5",
-  "mqtt_host": "do2786.messaging.internetofthings.ibmcloud.com",
-  "mqtt_u_port": 1883,
-  "mqtt_s_port": 8883,
-  "http_host": "do2786.internetofthings.ibmcloud.com",
-  "org": "do2786",
-  "apiKey": "a-do2786-rmcfhb5u5k",
-  "apiToken": "IHQfh13-M9B*ueuvgL"
-};
+  };
 
 // Settings for registerting edge device on IOT platform
 // FILL IN, change any information necessary to setup the edge device
